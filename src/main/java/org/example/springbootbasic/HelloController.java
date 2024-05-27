003a8546af7bc9bbb18b3c3f025f36a2b1e47983
hlundstad <hlundstad@gmail.com>
2024-05-27 15:00:01 +0200

    added logging

--- src/main/java/org/example/springbootbasic/HelloController.java
+++ src/main/java/org/example/springbootbasic/HelloController.java
@@ -1,14 +1,16 @@
 package org.example.springbootbasic;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
+        log.info("HelloController.index() called");
         return "Greetings from Spring Boot!";
     }
 
